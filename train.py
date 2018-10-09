e569e6b24b6a42543e8c6ea6b0b8240b99079fa0
fangyuxin <2857653516@qq.com>
2018-10-09 23:03:54 +0800

    train写了一部分.

---
+++ train.py
@@ -0,0 +1,49 @@
+
+import torch
+from utils import *
+from config import *
+from models import *
+import torch.nn as nn
+from data import *
+from utils import *
+
+
+def train(**kwarg):
+
+    '''
+        1.初始化config, 更新相关参数.
+        2.初始化visdom环境.
+        3.初始化device.
+        4.初始化model.
+        5.初始化dataset, dataloader, augmentation.
+    '''
+
+    # 初始化config, 更新相关参数.
+    cfg.parse(kwarg)
+
+    # 初始化visdom环境.
+    vis = Visualizer(cfg.env)
+
+    # 初始化device.
+    device = cfg.device
+
+    # 初始化model.
+    model = getattr(models, cfg.model)(cfg.in_channel, cfg.out_channel).to(device)
+    model = nn.DataParallel(model)
+
+    # 初始化dataset, dataloader, augmentation.
+    train_data = NeuronDataset(cfg.dataset_root, phase='train', \
+                               aug_dict=cfg.aug_dict)
+
+    train_loader = DataLoader(train_data, batch_size=cfg.batch_size, \
+                              num_workers=cfg.num_workers)
+
+    # 初始化criterion.
+    criterion = get_criterion(cfg)
+
+    # 初始化optimizer.
+    optimizer = get_optimizer(cfg)(model.parameters(), )
+
+
+
+
